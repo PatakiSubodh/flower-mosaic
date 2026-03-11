235053e66c040882f388b1c61a8407e6c43261b5
Subodh Pataki <spataki@greywiz.com>
2026-03-11 16:57:35 +0530

    deploy ver1

--- lib/flowerTree.ts
+++ lib/flowerTree.ts
@@ -1,6 +1,13 @@
 import { kdTree } from "kd-tree-javascript";
-import flowersData from "@/data/flower-metadata.json";
+// import flowersData from "@/data/flower-metadata.json";
 import { LabColor, FlowerMeta } from "./types";
+
+import fs from "fs"
+import path from "path"
+
+const flowersData = JSON.parse(
+  fs.readFileSync(path.join(process.cwd(), "data/flower-metadata.json"), "utf8")
+)
 
 function weightedDistance(a: LabColor, b: LabColor): number {
   const dL = a.L - b.L;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingExcludes: {
+    "*": [
+      "./public/tiles/**/*",
+      "./public/flowers-original/**/*"
+    ]
+  },
+  serverExternalPackages: ["sharp"]
 };
+
 
 export default nextConfig;
